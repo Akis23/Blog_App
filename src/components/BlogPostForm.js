314390282fcd618cabc104fd90d28923fe505d67
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-18 13:21:19 +0300

    EditSceen Edit content and title

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const BlogPostForm = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogPostForm;
